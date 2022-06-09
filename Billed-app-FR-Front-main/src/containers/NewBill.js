2c27b3a371c325c8f559455d671a7daf79f7b155
Cecilia Giusti <ceciliagiusti2b@gmail.com>
2022-06-09 11:43:06 +0200

    Fix: update files accepted with html5

--- Billed-app-FR-Front-main/src/containers/NewBill.js
+++ Billed-app-FR-Front-main/src/containers/NewBill.js
@@ -20,40 +20,31 @@ export default class NewBill {
   handleChangeFile = (e) => {
     e.preventDefault();
 
-    const extendAccepted = new RegExp(".(jpg|jpeg|png)$");
-    if (
-      extendAccepted.test(
-        this.document.querySelector(`input[data-testid="file"]`).files[0].name
-      )
-    ) {
-      const file = this.document.querySelector(`input[data-testid="file"]`)
-        .files[0];
-      const filePath = e.target.value.split(/\\/g);
-      const fileName = filePath[filePath.length - 1];
+    const file = this.document.querySelector(`input[data-testid="file"]`)
+      .files[0];
+    const filePath = e.target.value.split(/\\/g);
+    const fileName = filePath[filePath.length - 1];
 
-      const formData = new FormData();
-      const email = JSON.parse(localStorage.getItem("user")).email;
-      formData.append("file", file);
-      formData.append("email", email);
+    const formData = new FormData();
+    const email = JSON.parse(localStorage.getItem("user")).email;
+    formData.append("file", file);
+    formData.append("email", email);
 
-      this.store
-        .bills()
-        .create({
-          data: formData,
-          headers: {
-            noContentType: true,
-          },
-        })
-        .then(({ fileUrl, key }) => {
-          console.log(fileUrl);
-          this.billId = key;
-          this.fileUrl = fileUrl;
-          this.fileName = fileName;
-        })
-        .catch((error) => console.error(error));
-    } else {
-      this.document.querySelector(`input[data-testid="file"]`).value = "";
-    }
+    this.store
+      .bills()
+      .create({
+        data: formData,
+        headers: {
+          noContentType: true,
+        },
+      })
+      .then(({ fileUrl, key }) => {
+        console.log(fileUrl);
+        this.billId = key;
+        this.fileUrl = fileUrl;
+        this.fileName = fileName;
+      })
+      .catch((error) => console.error(error));
   };
 
   handleSubmit = (e) => {
