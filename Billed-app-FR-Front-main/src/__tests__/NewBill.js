6bf91fe17b3f5edbec78b611664a1f2b286d019e
Cecilia Giusti <ceciliagiusti2b@gmail.com>
2022-06-09 11:54:34 +0200

    Tests: add tests for new bill page

--- Billed-app-FR-Front-main/src/__tests__/NewBill.js
+++ Billed-app-FR-Front-main/src/__tests__/NewBill.js
@@ -2,17 +2,93 @@
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom"
-import NewBillUI from "../views/NewBillUI.js"
-import NewBill from "../containers/NewBill.js"
+import "@testing-library/jest-dom";
+import { screen, fireEvent } from "@testing-library/dom";
+import userEvent from "@testing-library/user-event";
+import NewBillUI from "../views/NewBillUI.js";
+import { ROUTES_PATH, ROUTES } from "../constants/routes.js";
+import mockStore from "../__mocks__/store";
+import { localStorageMock } from "../__mocks__/localStorage.js";
+import router from "../app/Router.js";
+import NewBill from "../containers/NewBill.js";
 
+jest.mock("../app/store", () => mockStore);
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
-    })
-  })
-})
+    test("then the submit should be send", () => {
+      Object.defineProperty(window, "localStorage", {
+        value: localStorageMock,
+      });
+
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({
+          type: "Employee",
+        })
+      );
+
+      const root = document.createElement("div");
+      root.setAttribute("id", "root");
+      document.body.append(root);
+      router();
+
+      document.body.innerHTML = NewBillUI();
+
+      const newBillObject = new NewBill({
+        document,
+        onNavigate,
+        store: null,
+        localStorage: window.localStorage,
+      });
+
+      const formNewBill = screen.getByTestId("form-new-bill");
+      const handleSubmit = jest.fn((e) => newBillObject.handleSubmit(e));
+
+      formNewBill.addEventListener("submit", handleSubmit);
+      fireEvent.submit(formNewBill);
+
+      expect(handleSubmit).toHaveBeenCalled();
+    });
+
+    test("Then it should send a bill with the right extension", () => {
+      Object.defineProperty(window, "localStorage", {
+        value: localStorageMock,
+      });
+
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({
+          type: "Employee",
+        })
+      );
+
+      const root = document.createElement("div");
+      root.setAttribute("id", "root");
+      document.body.append(root);
+      router();
+
+      document.body.innerHTML = NewBillUI();
+
+      const newBillObject2 = new NewBill({
+        document,
+        onNavigate,
+        store: mockStore,
+        localStorage: window.localStorage,
+      });
+
+      const file = screen.getByTestId("file");
+      const handleChangeFile = jest.fn((e) =>
+        newBillObject2.handleChangeFile(e)
+      );
+
+      file.addEventListener("change", handleChangeFile);
+      fireEvent.change(file, {
+        target: {
+          files: [new File(["Test"], "test.png")],
+        },
+      });
+      expect(handleChangeFile).toHaveBeenCalled();
+    });
+  });
+});
